044312950838886be98d127dfaa468daac9b4c9d
sreddy17 <114898509+sreddy17@users.noreply.github.com>
2022-10-04 13:52:49 +1100

    Update TestADF1.sql

--- TestADF1.sql
+++ TestADF1.sql
@@ -3,48 +3,16 @@
 SET ANSI_NULLS ON
 GO
 
-
-
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-
-CREATE PROCEDURE [dbo].[LogIt] @logDateTime Datetime,@logSummary NVARCHAR(64),@logDetails NVARCHAR(Max)
+CREATE PROCEDURE [dbo].[TestIt] @logDateTime Datetime,@logSummary NVARCHAR(64),@logDetails NVARCHAR(Max)
 as
-
-
 
 SELECT  @logDateTime=ISNULL(@logDateTime,GETUTCDATE()) --IF @logDateTime is null then set as utcnow()
 --BEGIN
 --    Print('Test')
 --END
-
-
-
-
-INSERT INTO [dbo].[activityLog]
-           ([logSource]
-           ,[logSummary]
-           ,[logLevel],
-           [logDetails]
-           )
-     VALUES
-           ('source'
-           ,@logSummary
-           ,0
-           ,@logDetails
-           )
-
-
-
---DECLARE @MySmallDateTime smalldatetime = CURRENT_TIMESTAMP;
---DECLARE @MyDateTime datetime = GETUTCDATE();
---EXECUTE [dbo].[LogIt] @MyDateTime ,"logSummary","logDetails"
---EXECUTE [dbo].[LogIt] "EventName" ,"20220919T04:52" ,"EventDetail"
-
-
-
+SELECT * FROM [dbo].[activityLog]
 
 GO
