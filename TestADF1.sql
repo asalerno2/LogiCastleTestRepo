5012ed89f78ed622866cb8f86e811081246facb2
asalerno2 <104182714+asalerno2@users.noreply.github.com>
2022-10-04 14:22:04 +1100

    Update TestADF1.sql

--- TestADF1.sql
+++ TestADF1.sql
@@ -1,16 +1,9 @@
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE PROCEDURE [dbo].[TestIt01] @logDateTime Datetime,@logSummary NVARCHAR(64),@logDetails NVARCHAR(Max)
 as
 
-SELECT  @logDateTime=ISNULL(@logDateTime,GETUTCDATE()) --IF @logDateTime is null then set as utcnow()
+SELECT  @logDateTime=ISNULL(@logDateTime,GETUTCDATE())
 
 Print('Test')
 
-SELECT * FROM [dbo].[activityLog]
 
 GO
